521527fb7dc6b45857549aa2199c0c7f71ad8aad
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-01-18 21:06:32 -0500

    Enhance ESLint configuration with Airbnb and Prettier support, and update rules for TypeScript and Tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,51 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "airbnb-typescript/base",
+      "next/core-web-vitals",
+      "plugin:prettier/recommended",
+      "plugin:tailwindcss/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react", "@typescript-eslint", "prettier", "tailwindcss"],
+    parserOptions: {
+      ecmaVersion: "latest",
+
+      sourceType: "module",
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname,
+    },
+    rules: {
+      "@typescript-eslint/lines-between-class-members": "off",
+      "@typescript-eslint/no-throw-literal": "off",
+      quotes: [2, "double", { avoidEscape: true }],
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          tabWidth: 2,
+          trailingComma: "all",
+          printWidth: 100,
+          arrowParens: "always",
+          bracketSpacing: true,
+          jsxBracketSameLine: false,
+          jsxSingleQuote: false,
+          quoteProps: "as-needed",
+          useTabs: false,
+          semi: true,
+          endOfLine: "auto",
+        },
+      ],
+      "import/no-extraneous-dependencies": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "tailwindcss/classnames-order": "warn",
+      "tailwindcss/no-custom-classname": "warn",
+      "tailwindcss/no-contradicting-classname": "error",
+      "no-trailing-spaces": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
