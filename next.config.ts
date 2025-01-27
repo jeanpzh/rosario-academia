fea4a3833375bbdcd3f83552c3007bb6ec11028f
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-01-26 23:05:20 -0500

    Add remote image patterns, update dependencies, and enhance Tailwind CSS configuration with custom color variables

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+      },
+      {
+        protocol: "https",
+        hostname: "png.pngtree.com",
+      },
+    ],
+  },
 };
-
 export default nextConfig;
