cef349d468050e7500f058670545970df85e4674
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-01-30 03:21:09 -0500

    Remove athlete dashboard loading component

--- src/app/dashboard/athlete/loading.tsx
+++
@@ -1,7 +0,0 @@
-import DashboardSkeleton from "@/app/dashboard/athlete/components/DashboardSkeleton";
-
-import React from "react";
-
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
