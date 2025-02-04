097abb332c973045c303753ead4388f278fa447d
Tealbone <guerrerochristian085@gmail.com>
2025-02-04 00:09:08 -0500

    loading page for login

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,8 +1,30 @@
-import { Message } from "@/components/form-message";
+"use client";
+import React, { useEffect, useState } from "react";
+import LoadingPage from "@/app/dashboard/athlete/components/LoadingPage";
 import SignIn from "./SignIn";
+import { Message } from "@/components/form-message";
 
-export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
+export default function Login(props: { searchParams: Promise<Message> }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchParams, setSearchParams] = useState<Message | null>(null);
 
-  return <SignIn searchParams={searchParams} />;
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const params = await props.searchParams;
+      setSearchParams(params);
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [props.searchParams]);
+
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingPage />
+      ) : (
+        <SignIn searchParams={searchParams!} /> // Mostrar el formulario de inicio de sesión
+      )}
+    </div>
+  );
 }
