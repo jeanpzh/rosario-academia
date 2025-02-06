b7197904e7e5896c43b06255969a12e1759721e8
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-02-05 21:24:54 -0500

    Add DashboardLayout component and athlete links for improved navigation

---
+++ src/app/dashboard/athlete/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { DashboardSidebar } from "@/components/layout/dashboard/dashboard-sidebar";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import links from "@/app/dashboard/athlete/utils/athlete-links";
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return redirect("/sign-in");
+
+  return (
+    <div className="flex min-h-screen w-full max-md:flex max-md:flex-col">
+      <DashboardSidebar links={links} user={user.user_metadata} />
+      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+}
