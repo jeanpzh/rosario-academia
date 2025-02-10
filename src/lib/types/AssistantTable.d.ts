f5900c93242a0463bbf41d4a2914fd1bb6004069
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-02-09 20:32:07 -0500

    Enhance sign-up action with user role processing and error handling; add toast notification system and new type definitions for athletes and assistants.

---
+++ src/lib/types/AssistantTable.d.ts
@@ -0,0 +1,11 @@
+export type Assistant = {
+  id: string;
+  avatar_url: string;
+  first_name: string;
+  paternal_last_name: string;
+  maternal_last_name: string;
+  birth_date: string;
+  dni: string;
+  phone: string;
+  email: string;
+};
