803aa1d5ef5a4bbbad506e9174e6a265b4292020
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-02-09 20:26:18 -0500

    Enhance middleware to include athlete role handling and improve user redirection logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "./utils/supabase/server";
 import { updateSession } from "./utils/supabase/middleware";
+import { athleteMiddleware } from "./utils/athlete/middleware";
 
 export async function middleware(request: NextRequest) {
   try {
@@ -12,21 +13,27 @@ export async function middleware(request: NextRequest) {
 
     // Get the user
     const { data, error } = await supabase.auth.getUser();
+
     // If there is an error or no user, redirect to the sign-in page
     const user = data.user;
     if (error || !user) {
       return NextResponse.redirect(new URL("/sign-in", request.url));
     }
+
     // Check the user role
-    const { data: userProfile } = await supabase
+    const { data: userProfile, error: profileError } = await supabase
       .from("profiles")
       .select("role")
       .eq("id", user.id)
       .single();
+
     const userRole = userProfile?.role;
+    if (profileError || !userRole) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
     // Redirect to respective dashboard if the user presses "/dashboard" in the URL
-    if (error || !userRole) return NextResponse.redirect(new URL("/sign-in", request.url));
     if (request.nextUrl.pathname === "/dashboard") {
+      console.log("Redirecting to specific dashboard based on user role");
       if (userRole === "admin") {
         return NextResponse.redirect(new URL("/dashboard/admin", request.url));
       } else if (userRole === "deportista") {
@@ -34,10 +41,10 @@ export async function middleware(request: NextRequest) {
       } else if (userRole === "auxiliar_administrativo") {
         return NextResponse.redirect(new URL("/dashboard/auxiliar", request.url));
       } else {
-        // Si no coincide con ningún rol
         return NextResponse.redirect(new URL("/sign-in", request.url));
       }
     }
+
     // Redirect to the sign-in page if the user is not who they say they are
     if (request.nextUrl.pathname.startsWith("/dashboard/admin") && userRole !== "admin") {
       return NextResponse.redirect(new URL("/dashboard", request.url));
@@ -51,12 +58,16 @@ export async function middleware(request: NextRequest) {
     ) {
       return NextResponse.redirect(new URL("/dashboard", request.url));
     }
-
+    if (userProfile.role === "deportista") {
+      const athleteResponse = await athleteMiddleware(request, user);
+      if (athleteResponse) return athleteResponse;
+    }
     return NextResponse.next();
   } catch (error) {
-    console.error(error);
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 }
+
 // matcher for the middleware to run
 export const config = {
   matcher: [
@@ -64,5 +75,8 @@ export const config = {
     "/dashboard/admin/:path*",
     "/dashboard/athlete/:path*",
     "/dashboard/admin-auxiliar/:path*",
+    "/dashboard/athlete/profile/:path*",
+    "/dashboard/athlete/schedule/:path*",
+    "/dashboard/athlete/carnet/:path*",
   ],
 };
