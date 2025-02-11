91f5620f0c37a07a6520e171680c663549b285a1
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-02-10 19:17:01 -0500

    Add Cloudinary hostname to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.pinimg.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
